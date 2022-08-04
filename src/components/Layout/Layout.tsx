0390cf424daf38cc0e03f8dd4fd14fe7473168fe
PauloGustavo.lata <paulo.gustavo@mngs.com.br>
2022-08-04 13:29:41 -0300

    initial commit

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import { Container } from './styles';
+import React from 'react';
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <p>Welcome to layout</p>
+    </Container>
+  );
+}
+
+export default Layout;
